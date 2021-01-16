af8ae6eef6271151ee2ec35e03d3904c250f2519
CarterStefan <ssttefancarter@gmail.com>
2021-01-16 09:51:14 +0000

    finalised stripe payment implementation

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,4 +1,3 @@
-
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
@@ -12,8 +11,9 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
+    print('delete signals recieved')
     instance.order.update_total()
